9725a201efe0c28c7f5746b34f44e8fd7b2bb5a2
Mason Hart <mhart5892@gmail.com>
2025-02-03 20:28:08 -0600

    Testing the Brawl Stars API in Python

---
+++ test.py
@@ -0,0 +1,14 @@
+import os
+import requests
+
+file = open("api_key.txt","r")
+API_KEY = file.read()
+print(API_KEY)
+PLAYER_TAG = "28UY8YJY0"
+
+url = f"https://api.brawlstars.com/v1/players/%23{PLAYER_TAG}"
+headers = {"Authorization": f"Bearer {API_KEY}"}
+
+response = requests.get(url,headers)
+data = response.json()
+print(data["trophies"])
